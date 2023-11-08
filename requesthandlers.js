2a1338f32e801592e9dbc07390bb98a520ed43ab
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2023-11-08 13:24:25 +0530

    Add files via upload

--- requesthandlers.js
+++ requesthandlers.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcrypt";
 import userSchema from "./Schema/user.schema.js"
+import jwt from "jsonwebtoken";
+
+const { sign } = jwt;
 
 
 
@@ -24,5 +27,39 @@ export async function register(req,res){
         console.log(error);
        res.status(500).send("Error");
         
+    }
+ }
+
+ export async function login(req, res){
+    try {
+        let {username,password} = req.body;
+        if(username.length < 4 && password.length < 4){
+            return res.json("Invalid username or password");
+        }
+        let user = await userSchema.findOne({username});
+        if(!user){
+            return res.status(403).send("Invalid username or password");
+        }
+        let passCheck = await bcrypt.compare(password, user.password)
+        if(passCheck){
+            let token = await sign({
+                username: user.username,
+                id:user._id
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn:"24h"
+            })
+            return res.status(200).json({
+                msg:"Login successful",
+                token:token
+        })
+        }
+        return res.status(403).send("inavlid username or password");        
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error");
     }
  }
--- router.js
+++ router.js
@@ -4,6 +4,7 @@ import * as userHandlers from "./requesthandlers.js";
 
 const router = Router()
 
-router.route("/register").post(userHandlers.register)
+router.route("/register").post(userHandlers.register);
+router.route("/login").post(userHandlers.login);
 
 export default router;
