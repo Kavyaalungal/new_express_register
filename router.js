a9942212d2933b99ac92ac38a7ac62696f83fffd
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2023-11-09 08:46:38 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
 import * as userHandlers from "./requesthandlers.js";
+import auth from "./middleware/auth.js";
 
 const router = Router()
 
 router.route("/register").post(userHandlers.register);
 router.route("/login").post(userHandlers.login);
+router.route("/profile").get(auth,userHandlers.profile);
+router.route("/addNote").post(auth,userHandlers.addNote);
 
 export default router;
