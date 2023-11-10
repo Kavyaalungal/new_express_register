bfc132e0324549bf5fd9d9cd9c6d6835d8c3e012
Kavyaalungal <142202840+Kavyaalungal@users.noreply.github.com>
2023-11-10 08:42:31 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,6 +7,7 @@ export default  async function auth(req,res,next) {
     try {
         let token = req.headers.authorization.split(" ")[1];
         let user =  await verify(token, process.env.SECRET_KEY);
+        
         if(user){
             req.user = user;
             next();
